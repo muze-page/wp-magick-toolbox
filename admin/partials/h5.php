e4a90566967f00d03e8255ca509a912f0eb12959
Muze-mac <13581261474@163.com>
2023-08-10 17:24:09 +0800

    优化

--- admin/partials/h5.php
+++ admin/partials/h5.php
@@ -24,21 +24,22 @@ if (!class_exists('MaMi_H5')) {
 
             //获取开关
             $switch =  MaMi_Admin::get_config($home, 'switch');
-            if (!$switch) {
+            if ($switch) {
                 // 定义 REST API 端点（Endpoint）
                 add_action('rest_api_init', array(__CLASS__, 'mytheme_register_rest_endpoints'));
             }
         }
 
 
+        //接口网址http://localhost:10020/wp-json/carbon-fields/v1/h5-options
         public static function mytheme_register_rest_endpoints()
         {
             $request = 1;
             // Get theme options
             register_rest_route('carbon-fields/v1', 'h5-options', array(
                 'methods' => 'GET',
-                'callback' => self::mytheme_get_theme_options(),
                 //'callback' => array(__CLASS__, 'mytheme_get_theme_options'),
+                'callback' => array(__CLASS__, 'get_h5_options'),
                 // 权限控制
                 // 'permission_callback' => function () {
                 //     return current_user_can('manage_options');
@@ -85,13 +86,16 @@ if (!class_exists('MaMi_H5')) {
         //返回选项值
         public static function get_h5_options($request)
         {
+            //准备数据
             $home = self::$home;
             $contact = self::$contact;
 
+            //首页
             $slide =  MaMi_Admin::get_config($home, 'slide');
             $slide_all =  MaMi_Admin::get_config($home, 'slide_all');
             $more =  MaMi_Admin::get_config($home, 'more');
 
+            //联系
             $title =  MaMi_Admin::get_config($contact, 'title');
             $title_one =  MaMi_Admin::get_config($contact, 'title_one');
             $content_one =  MaMi_Admin::get_config($contact, 'content_one');
@@ -100,6 +104,19 @@ if (!class_exists('MaMi_H5')) {
             $brand_link =  MaMi_Admin::get_config($contact, 'brand_link');
             $brand_logo =  MaMi_Admin::get_config($contact, 'brand_logo');
             $introduce =  MaMi_Admin::get_config($contact, 'introduce');
+
+            $options['comm_h5_index_tone'] = $slide; //数字数组
+            $options['comm_h5_index_tone_cat'] = $slide_all; //字符串
+            $options['comm_h5_index_category'] = $more; //数字
+            $options['comm_h5_single_contact_title'] = $title;
+            $options['comm_h5_single_contact_one_title'] = $title_one;
+            $options['comm_h5_single_contact_one_content'] = $content_one;
+            $options['comm_h5_single_contact_two_title'] = $title_two;
+            $options['comm_h5_single_contact_two_content'] = $content_two;
+            $options['comm_h5_singel_featured_link'] = $brand_link;
+            $options['comm_h5_singel_featured_logo'] = $brand_logo;
+            $options['comm_h5_singel_featured_msg'] = $introduce;
+            return $options;
         }
     } //end
 }
--- magick-mixtrue.php
+++ magick-mixtrue.php
@@ -48,66 +48,6 @@ function run_magick_mixture()
 }
 run_magick_mixture();
 
-//$magick_tool = new Magick_Mixtrue_Tool;
-
-//echo '<h1>当前文章评论已打开</h1>';
-//$magick_tool->run_page_hook();
-/**
- * 为WordPress后台的文章、分类等显示ID
- */
-// 添加一个新的列 ID
-
-// 定义 REST API 端点（Endpoint）
-add_action('rest_api_init', 'mytheme_register_rest_endpoints');
-
-function mytheme_register_rest_endpoints()
-{
-    // Get theme options
-    register_rest_route('carbon-fields/v1', 'h5-options', array(
-        'methods' => 'GET',
-        'callback' => 'mytheme_get_theme_options',
-        // 权限控制
-        // 'permission_callback' => function () {
-        //     return current_user_can('manage_options');
-        // },
-    ));
-}
-
-//返回选项值
-function mytheme_get_theme_options($request)
-{
-    $fields = [
-        'comm_h5_index_tone',
-        'comm_h5_index_tone_cat',
-        'comm_h5_index_category',
-        'comm_h5_single_contact_title',
-        'comm_h5_single_contact_one_title',
-        'comm_h5_single_contact_one_content',
-        'comm_h5_single_contact_two_title',
-        'comm_h5_single_contact_two_content',
-        'comm_h5_singel_featured_link',
-        'comm_h5_singel_featured_logo',
-        'comm_h5_singel_featured_msg',
-    ];
-
-    $options = [];
-    foreach ($fields as $field) {
-        $options[$field] = carbon_get_theme_option($field);
-        //处理数组
-        if ($field == "comm_h5_index_tone") {
-            //创建数组存储数据
-            $arr = [];
-            $sum = carbon_get_theme_option($field);
-            $arr = array_map(function ($obj) {
-                return $obj['id'];
-            }, $sum);
-            //转成数组
-            $intArray = array_map('intval', $arr);
-            $options[$field] = $intArray;
-        }
-    }
-    return $options;
-}
 
 
 
