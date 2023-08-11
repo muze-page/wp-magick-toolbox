428fdceb8069d604ff547d4808b393533dc9f282
Muze-mac <13581261474@163.com>
2023-08-11 19:12:55 +0800

    优化并开新项目

---
+++ vite/b2-shop/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import Tabs from "./components/tabs.vue";
+</script>
+
+<template>
+  <Tabs />
+  
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite/b2-shop/src/components/count/today.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+ 今天
+</template>
+
+<style scoped>
+
+</style>
