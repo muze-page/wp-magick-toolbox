8f21de9dcb1e1e13a80a542eb33946ff05b219e6
Muze-mac <13581261474@163.com>
2024-05-14 13:53:43 +0800

    完善提示

--- admin/partials/page/exterior/copy_pop_up.php
+++ admin/partials/page/exterior/copy_pop_up.php
@@ -2,8 +2,7 @@
 
 /**
  * 效果：复制时进行弹窗提示
- * 来源1：https://www.liitk.com/1098.html
- * 来源2：https://www.dqzboy.com/4672.html
+ * 来源1：https://www.dqzboy.com/4672.html
  */
 if (!class_exists('Npcink_Page_Copy_Pop_Up')) {
     class Npcink_Page_Copy_Pop_Up
@@ -38,7 +37,29 @@ if (!class_exists('Npcink_Page_Copy_Pop_Up')) {
         //复制成功提醒  
         public static function jiub()
         {
-            echo '<script>document.body.oncopy = function() { swal("复制成功！", "转载请务必保留原文链接，申明来源，谢谢合作！！","success");};</script>';
+            ?>
+            <script>
+            document.addEventListener('copy', function (event) {
+                // 获取复制的文本内容
+                var copiedText = window.getSelection().toString();
+    
+                // 在页面上显示提示信息
+                swal("复制成功！", "转载请务必保留原文链接，申明来源，谢谢合作！！", "success");
+    
+                // 将复制的文本内容写入剪贴板
+                navigator.clipboard.writeText(copiedText)
+                    .then(function () {
+                        //console.log('已成功将文本写入剪贴板！');
+                    })
+                    .catch(function (error) {
+                        console.error('写入剪贴板失败:', error);
+                    });
+    
+                // 阻止默认的复制行为，可以选择阻止或者允许，默认为允许
+                // event.preventDefault();
+            });
+            </script>
+            <?php
         }
 
         //加载资源
@@ -57,7 +78,7 @@ if (!class_exists('Npcink_Page_Copy_Pop_Up')) {
                 plugin_dir_url(__FILE__) . 'project/sweetalert/sweetalert.min.js',
                 array(),
                 MAGICK_MIXTURE_VERSION,
-                false
+                true
             );
         }
     }
