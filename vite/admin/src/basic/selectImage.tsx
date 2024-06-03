03dc1aefa6aefceae488ea1bde1ad45cfa471859
Muze-mac <13581261474@163.com>
2024-06-03 17:00:58 +0800

    完善图片选中

--- vite/admin/src/basic/selectImage.tsx
+++ vite/admin/src/basic/selectImage.tsx
@@ -1,6 +1,8 @@
 //基础组件 - 选中媒体库图片
-import { Input, Space, Button, Modal, List, Image } from "antd";
+import { Input, Space, Button, Modal, List, Radio } from "antd";
 import { useState } from "react";
+import type { RadioChangeEvent } from "antd";
+
 const SelectImage: React.FC = (props: any) => {
   //弹窗
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,13 +12,12 @@ const SelectImage: React.FC = (props: any) => {
 
   const getMediaData = async () => {
     //准备网址
-    const site = url_site + "/wp-json/wp/v2/media";
+    const site = url_site + "/wp-json/wp/v2/media?per_page=12";
 
     try {
       const response = await axios.get(site);
-        console.log(response.data);
-      //取前10张图片
-      const data = response.data.slice(0, 8);
+      //console.log(response.data);
+      const data = response.data;
       setMediaImage(data);
     } catch (error: any) {
       console.error("出错：" + error.message);
@@ -29,16 +30,29 @@ const SelectImage: React.FC = (props: any) => {
     //获取数据并存储
     getMediaData();
   };
+
+  //确定按钮
   const handleOk = () => {
     setIsModalOpen(false);
-    console.log("确定");
+    //传递选中的图片
+    props.onChange(imageValue);
   };
 
+  //取消按钮
   const handleCancel = () => {
     setIsModalOpen(false);
-    console.log("取消");
+    // console.log("取消");
   };
   //接收传来的值
+
+  //选中
+  const [imageValue, setImageValue] = useState(1);
+
+  //选中方法
+  const onChange = (e: RadioChangeEvent) => {
+    console.log("radio checked", e.target.value);
+    setImageValue(e.target.value);
+  };
   return (
     <>
       <Space.Compact style={{ width: "100%" }}>
@@ -51,6 +65,7 @@ const SelectImage: React.FC = (props: any) => {
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
+        
       >
         <List
           grid={{
@@ -64,9 +79,11 @@ const SelectImage: React.FC = (props: any) => {
           }}
           dataSource={mediaImage}
           renderItem={(item: any) => (
-            <List.Item>
-              <Image  alt={item.slug} src={item.source_url} />
-            </List.Item>
+            <Radio.Group onChange={onChange} value={imageValue}>
+              <Radio  value={item.source_url}>
+                <img alt={item.slug} src={item.source_url} width={200} height={200}/>
+              </Radio>
+            </Radio.Group>
           )}
         />
       </Modal>
