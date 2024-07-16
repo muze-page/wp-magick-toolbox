23e6e26b173a8043c35f5cafb371414f825ee355
Muze-mac <13581261474@163.com>
2024-07-16 14:32:59 +0800

    优化结构中

--- vite/admin/src/components/shortcode/index.tsx
+++ vite/admin/src/components/shortcode/index.tsx
@@ -1,5 +1,6 @@
 import Compose from "@/components/shortcode/compose";
 import Pendant from "@/components/shortcode/pendant";
+import Demo from "@/components/shortcode/demo";
 
 const App: React.FC = () => {
   return (
@@ -7,6 +8,7 @@ const App: React.FC = () => {
       <div className="describe">短代码描述</div>
       <Compose />
       <Pendant />
+      <Demo />
     </>
   );
 };
