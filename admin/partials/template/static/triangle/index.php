c041d903d764d8fdfdba03fa9399ed4757eb046b
Muze-mac <13581261474@163.com>
2024-08-08 15:32:11 +0800

    优化三角立体效果

--- admin/partials/template/static/triangle/index.php
+++ admin/partials/template/static/triangle/index.php
@@ -8,6 +8,19 @@ if (!class_exists('Npcink_Template_Triangle')) {
     {
         public static function run()
         {
+            add_action('wp_enqueue_scripts', array(__CLASS__, 'theme_enqueue_styles'));
+        }
+
+        public static function theme_enqueue_styles()
+        {
+
+            $build_css =  plugin_dir_url(__DIR__) . 'triangle/triangle.css';
+            $style_css =  plugin_dir_url(__DIR__) . 'triangle/style.css';
+            // 如果当前页面模板是 template-aaa.php，则加载特定的 CSS 文件
+            if (is_page_template('template-triangle.php')) {
+                wp_enqueue_style(MAGICK_MIXTURE_NAME . '_triangle', $build_css, array(), MAGICK_MIXTURE_VERSION, 'all');
+                wp_enqueue_style(MAGICK_MIXTURE_NAME . '_triangle-style', $style_css, array(), MAGICK_MIXTURE_VERSION, 'all');
+            }
         }
     }
 }
--- admin/partials/template/static/triangle/template-triangle.php
+++ admin/partials/template/static/triangle/template-triangle.php
@@ -1,13 +1,53 @@
 <?php
-    get_header();
+get_header();
 // Custom template code goes here
-echo "立体三角";
+
 // 获取当前页面的标题
 $page_title = get_the_title();
-echo $page_title;
+
 
 // 获取当前页面的内容
-$page_content = apply_filters( 'the_content', get_post_field( 'post_content', get_the_ID() ) );
-echo $page_content;
+$page_content = apply_filters('the_content', get_post_field('post_content', get_the_ID()));
+
+?>
+<div class="triangle">
+    <!--标题-->
+    <header class="entry-header">
+        <h2 class="triangle-center entry-title">
+            <?php echo $page_title; ?>
+        </h2>
+    </header>
+
+
+
+
+    <div id="triangle" class="triangle-center">
+        <svg id="Layer_1" data-name="Layer 1" version="1.1" viewBox="0 0 2000 2000">
+            <polygon
+                class="cls-1"
+                points="928 781 1021 951 784.5 1371.97 1618 1371.97 1530.32 1544 509 1539 928 781"></polygon>
+            <polygon
+                class="cls-3"
+                points="1618 1371.97 784.5 1371.97 874.93 1211 1346 1211 923.1 456 1110.06 456 1618 1371.97"></polygon>
+            <g id="Layer_2" data-name="Layer 2">
+                <polygon
+                    class="cls-2"
+                    points="418 1372.74 509 1539 928 781 1162.32 1211 1346 1211 923.1 456 418 1372.74"></polygon>
+            </g>
+        </svg>
+    </div>
+
+    <!--内容-->
+    <div class="entry-content">
+        <?php echo $page_content; ?>
+    </div>
+</div>
+
+
+
+
+<?php
+
+
 
 get_footer();
